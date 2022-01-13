3fafd8ca55bbf746f01d221af3f9a345d5cc7668
imhuis <yjh19980105@gmail.com>
2022-01-13 10:57:51 +0800

    fix: remove cloud module

--- src/main/java/com/imhui/security/SecurityServerApplication.java
+++ src/main/java/com/imhui/security/SecurityServerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @description:
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SecurityServerApplication {
 
     public static void main(String[] args) {
